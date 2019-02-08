5bc40575d2bfe3a5489c1086a8192e35ef93dc05
Anker <suanlatudousibufangcu@gmail.com>
2019-02-09 05:23:51 +0800

    Create date_type.py

---
+++ part1/1_data_type/date_type.py
@@ -0,0 +1,16 @@
+
+#声明一个变量
+
+first_number = 1
+print(first_number)
+print(type(first_number))
+
+second_float_number = 1.2
+print(second_float_number)
+print(type(second_float_number))
+
+#布尔型 True False
+
+third_boolean = False
+print(third_boolean)
+print(type(third_boolean))
