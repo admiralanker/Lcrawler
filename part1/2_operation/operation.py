5a1857286b77ad513cceca23543841239eb92fe8
Anker <suanlatudousibufangcu@gmail.com>
2019-02-09 05:24:03 +0800

    Create operation.py

---
+++ part1/2_operation/operation.py
@@ -0,0 +1,94 @@
+# '''
+# 基本运算符
+# +  -  *  /
+# '''
+#
+# #加法运算
+#
+# first_number = 5
+# second_number = 6
+# result = first_number + second_number
+# print(result)
+#
+# #减法运算
+#
+# first_number = 7
+# second_number = 9
+# result = first_number - second_number
+# print(result)
+#
+# #乘法运算
+#
+# first_number = 2
+# second_number = 3
+# result = first_number * second_number
+# print(result)
+#
+# #除法运算
+#
+# first_number = 21
+# second_number = 3
+# result = first_number / second_number
+# print(result)
+# print(type(result))
+
+'''
+算术运算符
+'''
+
+#取模运算
+
+# i = 10
+# j = 3
+# z = i % j
+# print(z)
+#
+# #幂运算
+#
+# z = i ** j
+# print(z)
+# print(type(z))
+#
+# #取整除运算
+#
+# z = i // j
+# print(z)
+
+'''
+赋值运算符
+'''
+
+#加等于
+i = 1
+i += 5
+print(i)
+
+#减等于
+i = 2
+i -= 3
+print(i)
+
+#乘等于
+i = 3
+i *= 6
+print(i)
+
+#除等于
+i = 9
+i /= 4
+print(i)
+
+#模等于
+i = 4
+i %= 5
+print(i)
+
+#冥等于
+i = 7
+i **= 2
+print(i)
+
+#取整除等于
+i = 8
+i //= 6
+print(i)
