646c202e31d6df1ccb96c30cfc33ddda59ff5812
Anker <suanlatudousibufangcu@gmail.com>
2019-02-10 03:54:38 +0800

    Update function_name_stu.py

--- part4/function_name_stu.py
+++ part4/function_name_stu.py
@@ -1,4 +1,17 @@
 '''
 __name__
-
+只有在本模块启动时，__name__等于__main__
+在什么时候使用
+1、今天作为这个模块的入口，在其他语言中，叫做main函数
+2、也可以作为调试使用，原因是在其他模块调用本模块时，__name__ == 'main'
+判断结果为False，所以就不会执行
 '''
+
+
+def my_print():
+    print(__name__)
+
+if __name__ == '__main__':
+    my_print()
+    print(1)
+    print(__name__)
