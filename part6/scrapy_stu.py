403fea976f3a8004303f038032d2136955584ad8
Anker <suanlatudousibufangcu@gmail.com>
2019-02-13 01:18:37 +0800

    Update scrapy_stu.py

--- part6/scrapy_stu.py
+++ part6/scrapy_stu.py
@@ -1,6 +1,15 @@
 '''
+光IDE安装scrapy不行，还需要在项目所在目录安装
 安装：pip install scrapy
 '''
 
 import scrapy
+
+html = scrapy.Request('http://stockpage.10jqka.com.cn/600004/company/#detail')
+print(html)
+
+'''
+创建scrapy工程
+scrapy startproject spider_name
+'''
 
